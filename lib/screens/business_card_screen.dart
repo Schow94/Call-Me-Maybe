78fb1cbb40c0673a0187bb999e6eb02aa583ab32
schow-caredx <schow@caredx.com>
2022-07-06 22:07:03 -0700

    Work on BusinessCard Screen

--- lib/screens/business_card_screen.dart
+++ lib/screens/business_card_screen.dart
@@ -5,8 +5,48 @@ class BusinessCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('Business Card Screen'),
+    return FractionallySizedBox(
+      heightFactor: 0.95,
+      widthFactor: 0.95,
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image.asset(
+                'assets/images/stevejobs.jpeg',
+              ),
+            ),
+            Text(
+              'Steve Jobs',
+              style: Theme.of(context).textTheme.headline2,
+            ),
+            Text(
+              'Technology Extraordinaire',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Text(
+              '(415) 359-3432',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  'github.com/SteveJobs',
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+                Text(
+                  'steve.jobs@apple.com',
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
