effceee19f873da64d854444f01bfb94cb4de544
Schow94 <Schow94@gmail.com>
2022-07-12 08:17:39 -0700

    Move Predictor State to a Model

---
+++ lib/models/phrase.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+final textOptions = [
+  'Unlikely but possible',
+  "There's a chance",
+  "Umm I guess we'll have to wait and see",
+  "Only time will tell",
+  "I'm confident they will",
+  "Absolutely, 100%",
+  "I feel like they should, but don't take my word for it",
+  "If you believe it, it will happen"
+];
+
+class Phrase {
+  final Random random = Random();
+  final int randNum;
+  String _randPhrase = 'Unlikely but possible';
+
+  // Constructor - Initialize randNum to 0
+  Phrase({this.randNum = 0});
+
+  /*
+    - Get a random # & use it to pick a random phrase from textOptions
+  */
+  void randomize() {
+    var randNum = random.nextInt(textOptions.length);
+    _randPhrase = textOptions[randNum];
+  }
+
+  /*
+    - Getter: Returns randPhrase
+  */
+  String get randPhrase => _randPhrase;
+}
--- lib/screens/predictor_screen.dart
+++ lib/screens/predictor_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 import 'package:project3/widgets/random_text.dart';
+import 'package:project3/models/phrase.dart';
 
 class Predictor extends StatefulWidget {
   const Predictor({Key? key}) : super(key: key);
@@ -11,23 +12,10 @@ class Predictor extends StatefulWidget {
 }
 
 class _PredictorState extends State<Predictor> {
-  var rand = Random().nextInt(4);
-
-  final textOptions = [
-    'Unlikely but possible',
-    "There's a chance",
-    "Umm I guess we'll have to wait and see",
-    "Only time will tell",
-    "I'm confident they will",
-    "Absolutely, 100%",
-    "I feel like they should, but don't take my word for it",
-    "If you believe it, it will happen"
-  ];
+  final phrase = Phrase();
 
   @override
   Widget build(BuildContext context) {
-    String text = textOptions[rand];
-
     return Container(
       margin: EdgeInsets.all(20),
       child: Column(
@@ -39,22 +27,18 @@ class _PredictorState extends State<Predictor> {
           ),
           SizedBox(height: 50),
           GestureDetector(
-            onTap: clickBtn,
+            onTap: () => clickBtn(),
             child: Text(
               'Ask a question..tap for the answer',
               style: Theme.of(context).textTheme.headline6,
             ),
           ),
           SizedBox(height: 50),
-          RandomText(text: textOptions[rand]),
+          RandomText(text: phrase.randPhrase),
         ],
       ),
     );
   }
 
-  void clickBtn() {
-    setState(() {
-      rand = Random().nextInt(textOptions.length);
-    });
-  }
+  void clickBtn() => setState(() => phrase.randomize());
 }
